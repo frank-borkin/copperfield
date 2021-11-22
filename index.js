ca11b98d021269a17710e0bb77626d6d56907057
Frank Borkin <borkinfr@gmail.com>
2021-11-22 23:06:53 +0000

    Count down from an hour

--- index.js
+++ index.js
@@ -13,17 +13,19 @@ server.listen(port, () => {
 
 // Routing
 app.use(express.static(path.join(__dirname, 'public')));
+timers = Array();
 
 io.on('connection', (socket) => {
     socket.on('register', (instance) => {
         console.log("registered " + instance);
+        timers[instance] = date.parse('01:00:00 AM', 'hh:mm:ss A');
         setInterval(sendStatus, 1000, instance);
     });
 
     function sendStatus(instance) {
-        const now = new Date();
+        timers[instance] = date.addMilliseconds(timers[instance], -500);
         socket.broadcast.emit('status' + instance, {
-            time: date.format(now, 'HH:mm:ss'),
+            time: date.format(timers[instance], 'HH:mm:ss'),
             bgType: "pic",
             bgPath: "backgrounds/21.jpg"
         });
