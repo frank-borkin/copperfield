818b5353f18c736f14c06b05b889a17370f40a72
Frank Borkin <borkinfr@gmail.com>
2021-11-23 18:43:26 +0000

    Start GM page

--- index.js
+++ index.js
@@ -15,11 +15,13 @@ server.listen(port, () => {
 // Routing
 app.use(express.static(path.join(__dirname, 'public')));
 timers = Array();
+state = Array();
 
 io.on('connection', (socket) => {
     socket.on('register', (instance, callback) => {
         const now = new Date();
         timers[instance] = date.addHours(now, 1);
+        state[instance] = 'reset';
         setInterval(sendStatus, 1000, instance);
         callback && callback({
             status: "ok",
@@ -28,10 +30,19 @@ io.on('connection', (socket) => {
         });
     });
 
+    socket.on('gm', (callback) => {
+        callback && callback({
+            status: "ok",
+            games: ['An Hour to Kill', 'The Crazy Cat Lady', 'Rob the Bank']
+        });
+    });
+
     function sendStatus(instance) {
-        var now = new Date();
-        var timeLeft = date.subtract(timers[instance], now).toSeconds();
-        socket.broadcast.emit('status' + instance, {
+        if (state[instance] == 'running') {
+            var now = new Date();
+            var timeLeft = date.subtract(timers[instance], now).toSeconds();
+        }
+        socket.broadcast.emit('status' + instance, { //broadcast to room
             time: padStart(Math.floor(timeLeft / 3600), 2, '0') + ':' + padStart(Math.floor(timeLeft % 3600 / 60), 2, '0') + ':' + padStart(Math.floor(timeLeft % 3600 % 60), 2, '0')
         });
     }
